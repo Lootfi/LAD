f941ccb1235d77bb6b01eb445493b7bbb977c292
Hind <selablindahind@gmail.com>
2022-05-27 18:53:52 +0100

    edit & help

---
+++ resources/views/help.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8" />
+ <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <title>FAQ</title>
+ <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
+</head>
+
+<body>
+ <div class="container">
+  <h1 class="title">Frequenty Asked Questions</h1>
+  <main class="accordion">
+   <div class="faq-img">
+    <img src="img/faq.svg" alt="" class="accordion-img" />
+   </div>
+   <div class="content-accordion" style="    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 42vh;">
+    <div class="question-answer">
+     <div class="question">
+      <h3 class="title-question">What is the best way to use LAD?</h3>
+      <button class="question-btn">
+       <span class="up-icon">
+        <i class="fas fa-chevron-up"></i>
+       </span>
+       <span class="down-icon">
+        <i class="fas fa-chevron-down"></i>
+       </span>
+      </button>
+     </div>
+     <div class="answer">
+      <p>
+       The best way to use LAD is the way which helps your students
+       learn most efficiently. LAD was designed on the principles of
+       Social constructionism - see the page on Pedagogy for more
+       information - but you do not have to use LAD in that way if it
+       is not appropriate to your needs.
+      </p>
+     </div>
+    </div>
+
+    <div class="question-answer">
+     <div class="question">
+      <h3 class="title-question">
+       Can LAD be used in different subjects?
+      </h3>
+      <button class="question-btn">
+       <span class="up-icon">
+        <i class="fas fa-chevron-up"></i>
+       </span>
+       <span class="down-icon">
+        <i class="fas fa-chevron-down"></i>
+       </span>
+      </button>
+     </div>
+     <div class="answer">
+      <p>
+       Yes. LAD is used in a variety of fields. Users in some subject
+       areas may benefit further from standard or compatible
+       third-party modules and plugins:
+      </p>
+     </div>
+    </div>
+    <div class="question-answer">
+     <div class="question">
+      <h3 class="title-question">
+       Can I track and search what people do and post in LAD?
+      </h3>
+      <button class="question-btn">
+       <span class="up-icon">
+        <i class="fas fa-chevron-up"></i>
+       </span>
+       <span class="down-icon">
+        <i class="fas fa-chevron-down"></i>
+       </span>
+      </button>
+     </div>
+     <div class="answer">
+      <p>
+       Assuming you are a teacher and it is your course, yes. LAD Admin
+       can do that for the entire site.
+      </p>
+     </div>
+    </div>
+    <div class="question-answer">
+     <div class="question">
+      <h3 class="title-question">
+       Can LAD be used in different subjects?
+      </h3>
+      <button class="question-btn">
+       <span class="up-icon">
+        <i class="fas fa-chevron-up"></i>
+       </span>
+       <span class="down-icon">
+        <i class="fas fa-chevron-down"></i>
+       </span>
+      </button>
+     </div>
+     <div class="answer">
+      <p>Yes. LAD is used in a variety of fields.</p>
+     </div>
+    </div>
+   </div>
+  </main>
+ </div>
+ <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
